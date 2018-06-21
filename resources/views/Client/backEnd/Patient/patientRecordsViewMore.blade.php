f93d652611cc7521f18f3f91a4f37bcd0b6f308c
sirbobz <bobcertified@gmail.com>
2018-06-21 12:43:10 +0300

    updated the patient details view

--- resources/views/Client/backEnd/Patient/patientRecordsViewMore.blade.php
+++ resources/views/Client/backEnd/Patient/patientRecordsViewMore.blade.php
@@ -104,10 +104,10 @@ Patient Record
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Add Patient</h4>
+                        <h4 class="modal-title">Update Records</h4>
                     </div>
                     <div class="modal-body">
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/register-patient') }}">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/update-patient-details') }}">
                             {{ csrf_field() }}
 
 
@@ -126,6 +126,8 @@ Patient Record
                                     @endif
                                 </div>
                             </div>
+
+                            <input type="hidden" name="patient_meta_data_id" value="{{$data->id}}">
 
                             <div class="form-group{{ $errors->has('second_name') ? ' has-error' : '' }}">
                                 <label for="second_name" class="col-md-4 control-label">Second Name </label>
@@ -155,13 +157,178 @@ Patient Record
                                 </div>
                             </div>
 
+                            <p class="col-md-offset-6">Contact Details</p>
+
+                            <div class="form-group{{ $errors->has('cell_phone') ? ' has-error' : '' }}">
+                                <label for="cell_phone" class="col-md-4 control-label">Phone </label>
+
+                                <div class="col-lg-6 col-md-8 col-sm-12">
+                                    <input id="cell_phone" type="number" class="form-control" name="cell_phone" value="{{ old('cell_phone') }}" required>
+
+                                    @if ($errors->has('cell_phone'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('cell_phone') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-4 control-label">Email </label>
+
+                                <div class="col-lg-6 col-md-8 col-sm-12">
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('alternative_cell_number') ? ' has-error' : '' }}">
+                                <label for="alternative_cell_number" class="col-md-4 control-label"> Alt Phone </label>
+
+                                <div class="col-lg-6 col-md-8 col-sm-12">
+                                    <input id="alternative_cell_number" type="number" class="form-control" name="alternative_cell_number" value="{{ old('alternative_cell_number') }}" required>
+
+                                    @if ($errors->has('alternative_cell_number'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('alternative_cell_number') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                          <p class="col-md-offset-6">Location Details</p>
+
+                            <div class="form-group{{ $errors->has('village') ? ' has-error' : '' }}">
+                                <label for="village" class="col-md-4 control-label">Village </label>
+
+                                <div class="col-lg-6 col-md-8 col-sm-12">
+                                    <input id="village" type="text" class="form-control" name="village" value="{{ old('village') }}" required>
+
+                                    @if ($errors->has('village'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('village') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('ward') ? ' has-error' : '' }}">
+                                <label for="ward" class="col-md-4 control-label"> Ward </label>
+
+                                <div class="col-lg-6 col-md-8 col-sm-12">
+                                    <input id="ward" type="text" class="form-control" name="ward" value="{{ old('ward') }}" required>
+
+                                    @if ($errors->has('ward'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('ward') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('subcounty') ? ' has-error' : '' }}">
+                                <label for="subcounty" class="col-md-4 control-label"> Sub County </label>
+
+                                <div class="col-lg-6 col-md-8 col-sm-12">
+                                    <input id="subcounty" type="text" class="form-control" name="subcounty" value="{{ old('subcounty') }}" required>
+
+                                    @if ($errors->has('subcounty'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('subcounty') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
 
 
+                            <div class="form-group{{ $errors->has('county') ? ' has-error' : '' }}">
+                                <label for="county" class="col-md-4 control-label">County </label>
+
+                                <div class="col-lg-6 col-md-8 col-sm-12">
+                                    <input id="county" type="text" class="form-control" name="county" value="{{ old('county') }}" required>
+
+                                    @if ($errors->has('county'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('county') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <p class="col-md-offset-6">Next of Kin Details</p>
+
+                            <div class="form-group{{ $errors->has('next_of_kin_name') ? ' has-error' : '' }}">
+                                <label for="next_of_kin_name" class="col-md-4 control-label">Kin Name </label>
+
+                                <div class="col-lg-6 col-md-8 col-sm-12">
+                                    <input id="next_of_kin_name" type="text" class="form-control" name="next_of_kin_name" value="{{ old('next_of_kin_name') }}" required>
+
+                                    @if ($errors->has('next_of_kin_name'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('next_of_kin_name') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('next_of_kin_phone') ? ' has-error' : '' }}">
+                                <label for="next_of_kin_phone" class="col-md-4 control-label"> Kin Phone </label>
+
+                                <div class="col-lg-6 col-md-8 col-sm-12">
+                                    <input id="next_of_kin_phone" type="number" class="form-control" name="next_of_kin_phone" value="{{ old('next_of_kin_phone') }}" required>
+
+                                    @if ($errors->has('next_of_kin_phone'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('next_of_kin_phone') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            
+
+                            <div class="form-group{{ $errors->has('next_of_kin_id_number') ? ' has-error' : '' }}">
+                                <label for="next_of_kin_id_number" class="col-md-4 control-label">Kin ID </label>
+
+                                <div class="col-lg-6 col-md-8 col-sm-12">
+                                    <input id="next_of_kin_id_number" type="number" class="form-control" name="next_of_kin_id_number" value="{{ old('next_of_kin_id_number') }}" required>
+
+                                    @if ($errors->has('next_of_kin_id_number'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('next_of_kin_id_number') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <p class="col-md-offset-6">Enrollment Details</p>
+
+                            <div class="form-group{{ $errors->has('enrollment_number') ? ' has-error' : '' }}">
+                                <label for="enrollment_number" class="col-md-4 control-label"> Enrollment Number </label>
+
+                                <div class="col-lg-6 col-md-8 col-sm-12">
+                                    <input id="enrollment_number" type="text" class="form-control" name="enrollment_number" value="{{ old('enrollment_number') }}" required>
+
+                                    @if ($errors->has('enrollment_number'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('enrollment_number') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+
+                           <br>
                             <div class="form-group">
                                 <div class="col-lg-6 col-md-8 col-sm-12 col-md-offset-4">
                                 <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal">Close</button>
                                     <button type="submit" class="btn btn-facebook btn-flat pull-right">
-                                        Save
+                                        Update
                                     </button>
                                 </div>
                             </div>
