b18f7ad178eef9462adc6d5e23ca91407d3a9516
sirbobz <bobcertified@gmail.com>
2018-06-21 15:05:43 +0300

    updated the seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Department;
+use App\Models\DepartmentLevel;
 
 class UsersGenerate extends Seeder
 {
@@ -11,5 +14,45 @@ class UsersGenerate extends Seeder
      */
     public function run()
     {
+ 
+        $level = new DepartmentLevel;
+        $level->level_id = 1;
+        $level->save();
+
+        $level = new DepartmentLevel;
+        $level->level_id = 2;
+        $level->save();
+
+
+    	$department = new Department;
+        $department->name = "Base Organization";
+		$department->level_id = 1;
+		$department->save();
+
+		$department = new Department;
+        $department->name = "Department";
+		$department->level_id = 2;
+		$department->save();
+
+
+		$user = new User;
+        $user->email = "admin@admin.com";
+		$user->depart_id = 1;
+        $user->first_name = "Admin";
+		$user->last_name = "Account";
+	    $user->verified = 1;
+		$user->save();
+
+
+		$user = new User;
+        $user->email = "depart@depart.com";
+		$user->depart_id = 2;
+        $user->first_name = "Test";
+		$user->last_name = "User";
+	    $user->verified = 1;
+		$user->save();
+
+       
+    }
 
 }
