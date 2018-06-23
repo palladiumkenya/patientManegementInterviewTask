d4f0145e3fcac42149a6e4306942e3169f55f0c4
U-MOBILEACCORD\gkoros <gkoros@gkoros.mobileaccord.local>
2018-06-24 00:32:30 +0300

    Home page customization

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import {Component, ElementRef, OnInit, Renderer} from '@angular/core';
+import {NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 
 import { Account, LoginModalService, Principal } from '../shared';
+import {Router} from '@angular/router';
+import {StateStorageService} from '../shared/auth/state-storage.service';
+import {LoginService} from '../shared/login/login.service';
 
 @Component({
     selector: 'jhi-home',
@@ -15,12 +18,23 @@ import { Account, LoginModalService, Principal } from '../shared';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
+    authenticationError: boolean;
+    password: string;
+    rememberMe: boolean;
+    username: string;
+    credentials: any;
 
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
+        private eventManager: JhiEventManager,
+        private loginService: LoginService,
+        private stateStorageService: StateStorageService,
+        private elementRef: ElementRef,
+        private renderer: Renderer,
+        private router: Router
     ) {
+        this.credentials = {};
     }
 
     ngOnInit() {
@@ -43,6 +57,42 @@ export class HomeComponent implements OnInit {
     }
 
     login() {
-        this.modalRef = this.loginModalService.open();
+        this.loginService.login({
+            username: this.username,
+            password: this.password,
+            rememberMe: this.rememberMe
+        }).then(() => {
+            this.authenticationError = false;
+            // this.activeModal.dismiss('login success');
+            if (this.router.url === '/register' || (/^\/activate\//.test(this.router.url)) ||
+                (/^\/reset\//.test(this.router.url))) {
+                this.router.navigate(['']);
+            }
+
+            this.eventManager.broadcast({
+                name: 'authenticationSuccess',
+                content: 'Sending Authentication Success'
+            });
+
+            // // previousState was set in the authExpiredInterceptor before being redirected to login modal.
+            // // since login is succesful, go to stored previousState and clear previousState
+            const redirect = this.stateStorageService.getUrl();
+            if (redirect) {
+                this.stateStorageService.storeUrl(null);
+                this.router.navigate([redirect]);
+            }
+        }).catch(() => {
+            this.authenticationError = true;
+        });
+    }
+
+    requestResetPassword() {
+        // this.activeModal.dismiss('to state requestReset');
+        this.router.navigate(['/reset', 'request']);
+    }
+
+    register() {
+        // this.activeModal.dismiss('to state register');
+        this.router.navigate(['/register']);
     }
 }
--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRouteSnapshot, NavigationEnd } from '@angular/router';
 
-import { JhiLanguageHelper } from '../../shared';
+import { JhiLanguageHelper, Principal } from '../../shared';
 
 @Component({
     selector: 'jhi-main',
-    templateUrl: './main.component.html'
+    templateUrl: './main.component.html',
+    styleUrls: [
+        'main.css'
+    ]
 })
 export class JhiMainComponent implements OnInit {
+    account: Account;
 
     constructor(
         private jhiLanguageHelper: JhiLanguageHelper,
-        private router: Router
+        private router: Router,
+        private principal: Principal
     ) {}
 
     private getPageTitle(routeSnapshot: ActivatedRouteSnapshot) {
@@ -24,9 +29,21 @@ export class JhiMainComponent implements OnInit {
 
     ngOnInit() {
         this.router.events.subscribe((event) => {
+            console.log(this.router.url, 'Current URL');
             if (event instanceof NavigationEnd) {
                 this.jhiLanguageHelper.updateTitle(this.getPageTitle(this.router.routerState.snapshot.root));
             }
         });
+        this.principal.identity().then((account) => {
+            this.account = account;
+        });
+    }
+
+    userIsNotAuthenticated() {
+        return !this.principal.isAuthenticated();
+    }
+
+    isHomePage() {
+        return (this.router.url === '/' || this.router.url === '');
     }
 }
